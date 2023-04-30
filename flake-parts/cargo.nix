ffbea6b99145d9448c2cc68e6272c0f1006c1217
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-04-30 03:41:34 -0500

    build(nix): add injector package

--- flake-parts/cargo.nix
+++ flake-parts/cargo.nix
@@ -22,6 +22,11 @@
       pkgs.bacon
       # version control
       pkgs.cocogitto
+      # formatting
+      self'.packages.treefmt
+      # misc
+      pkgs.wasm-bindgen-cli
+      pkgs.miniserve
     ];
 
     # packages required for building the rust packages
@@ -46,16 +51,62 @@
 
       nativeBuildInputs = withExtraPackages [];
       LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath nativeBuildInputs;
-      SQLX_OFFLINE = true;
     };
 
     deps-only = craneLib.buildDepsOnly ({} // common-build-args);
 
-    packages = {
+    packages = let
+      buildWasmPackage = {
+        name,
+        wasm-bindgen-target ? "web",
+      }: let
+        underscore_name = pkgs.lib.strings.replaceStrings ["-"] ["_"] name;
+
+        cargo-derivation = craneLib.mkCargoDerivation ({
+            pname = name;
+            cargoArtifacts = deps-only;
+            cargoExtraArgs = "-p ${name} --target wasm32-unknown-unknown";
+            doCheck = false;
+            doInstallCargoArtifacts = false;
+
+            buildPhaseCargoCommand = ''
+              cargoBuildLog=$(mktemp cargoBuildLogXXXX.json)
+              cargoWithProfile build -p ${name} --target wasm32-unknown-unknown --message-format json-render-diagnostics > $cargoBuildLog
+
+              mkdir -p $out
+              cp -r target $out
+            '';
+          }
+          // common-build-args);
+
+        wasm-derivation = pkgs.stdenv.mkDerivation {
+          name = "${name}-wasm-bindgen";
+          buildInputs = [pkgs.wasm-bindgen-cli];
+          nativeBuildInputs = [pkgs.binaryen];
+          src = "";
+          buildCommand = ''
+            ${pkgs.wasm-bindgen-cli}/bin/wasm-bindgen \
+              ${cargo-derivation}/target/wasm32-unknown-unknown/release/${underscore_name}.wasm \
+              --out-dir $out \
+              --target ${wasm-bindgen-target} \
+
+            ${pkgs.binaryen}/bin/wasm-opt \
+              -Oz \
+              --output $out/${underscore_name}_bg.wasm \
+              $out/${underscore_name}_bg.wasm
+          '';
+        };
+      in
+        wasm-derivation;
+    in {
       cargo-doc = craneLib.cargoDoc ({
           cargoArtifacts = deps-only;
         }
         // common-build-args);
+
+      injector = buildWasmPackage {
+        name = "reform-injector";
+      };
     };
 
     checks = {
@@ -87,14 +138,6 @@
       shellHook = ''
         ${config.pre-commit.installationScript}
       '';
-    };
-
-    apps = {
-      cli = {
-        type = "app";
-        program = pkgs.lib.getBin self'.packages.cli;
-      };
-      default = apps.cli;
     };
   };
 }
